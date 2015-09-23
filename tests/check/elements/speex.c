201424609c38de1e84668c195b99a98513fbcaa5
Havard Graff <havard.graff@gmail.com>
2015-09-23 16:05:55 +0200

    speexdec: Handle streamheaders in caps/buffers better
    https://bugzilla.gnome.org/show_bug.cgi?id=755481

--- tests/check/elements/speex.c
+++ tests/check/elements/speex.c
@@ -91,15 +91,124 @@ GST_START_TEST (test_encoder_to_decoder_timestamp)
 }
 GST_END_TEST;
 
+GST_START_TEST (test_headers_in_caps)
+{
+  GstHarness * h = gst_harness_new ("speexdec");
+  gst_harness_add_src_parse (h, "audiotestsrc is-live=1 samplesperbuffer=320 ! "
+      "capsfilter caps=\"audio/x-raw,format=S16LE,rate=16000\" ! "
+      "speexenc", TRUE);
+
+  /* the first audio-buffer produces 3 packets:
+     streamheader, vorbiscomments and the encoded buffer */
+  gst_harness_src_crank_and_push_many (h, 1, 3);
+
+  /* verify the decoder produces exactly one decoded buffer */
+  gst_buffer_unref (gst_harness_pull (h));
+  fail_unless_equals_int (0, gst_harness_buffers_in_queue (h));
+
+  gst_harness_teardown (h);
+}
+GST_END_TEST;
+
+GST_START_TEST (test_headers_in_buffers)
+{
+  GstHarness * h = gst_harness_new ("speexdec");
+  gst_harness_set_src_caps_str (h, "audio/x-speex");
+
+  /* turn off forwarding to avoid getting caps from the src-harness,
+     and hence not accessing the streamheaders through the caps */
+  gst_harness_set_forwarding (h, FALSE);
+
+  gst_harness_add_src_parse (h, "audiotestsrc is-live=1 samplesperbuffer=320 ! "
+      "capsfilter caps=\"audio/x-raw,format=S16LE,rate=16000\" ! "
+      "speexenc", TRUE);
+
+  /* the first audio-buffer produces 3 packets:
+     streamheader, vorbiscomments and the encoded buffer */
+  gst_harness_src_crank_and_push_many (h, 1, 3);
+
+  /* verify the decoder produces exactly one decoded buffer */
+  gst_buffer_unref (gst_harness_pull (h));
+  fail_unless_equals_int (0, gst_harness_buffers_in_queue (h));
+
+  gst_harness_teardown (h);
+}
+GST_END_TEST;
+
+GST_START_TEST (test_headers_not_in_buffers)
+{
+  GstHarness * h = gst_harness_new ("speexdec");
+
+  gst_harness_add_src_parse (h, "audiotestsrc is-live=1 samplesperbuffer=320 ! "
+      "capsfilter caps=\"audio/x-raw,format=S16LE,rate=16000\" ! "
+      "speexenc", TRUE);
+
+  /* the first audio-buffer produces 3 packets:
+     streamheader, vorbiscomments and the encoded buffer */
+  gst_harness_src_crank_and_push_many (h, 1, 0);
+
+  /* now remove the streamheader and vorbiscomment */
+  gst_buffer_unref (gst_harness_pull (h->src_harness));
+  gst_buffer_unref (gst_harness_pull (h->src_harness));
+
+  /* and just push the encoded buffer to the decoder */
+  gst_harness_src_crank_and_push_many (h, 0, 1);
+
+  /* verify the decoder produces exactly one decoded buffer */
+  gst_buffer_unref (gst_harness_pull (h));
+  fail_unless_equals_int (0, gst_harness_buffers_in_queue (h));
+
+  gst_harness_teardown (h);
+}
+GST_END_TEST;
+
+GST_START_TEST (test_headers_from_flv)
+{
+  GstHarness * h = gst_harness_new ("speexdec");
+
+  /* set caps with the same streamheader flvdemux would add */
+  gst_harness_set_src_caps_str (h, "audio/x-speex, "
+      "streamheader=(buffer)< 5370656578202020312e312e3132000000000000000000000"
+      "00000000100000050000000803e0000010000000400000001000000ffffffff500000000"
+      "000000001000000000000000000000000000000, "
+      "0b0000004e6f20636f6d6d656e74730000000001 >, "
+      "rate=(int)16000, channels=(int)1");
+
+  /* turn off forwarding to avoid getting caps from the src-harness,
+     and force it to use the flv-streamheaders we have set in the caps */
+  gst_harness_set_forwarding (h, FALSE);
+
+  gst_harness_add_src_parse (h, "audiotestsrc is-live=1 samplesperbuffer=320 ! "
+      "capsfilter caps=\"audio/x-raw,format=S16LE,rate=16000\" ! "
+      "speexenc", TRUE);
+
+  /* the first audio-buffer produces 3 packets:
+     streamheader, vorbiscomments and the encoded buffer */
+  gst_harness_src_crank_and_push_many (h, 1, 3);
+
+  /* verify the decoder produces exactly one decoded buffer */
+  gst_buffer_unref (gst_harness_pull (h));
+  fail_unless_equals_int (0, gst_harness_buffers_in_queue (h));
+
+  gst_harness_teardown (h);
+}
+GST_END_TEST
+
 static Suite *
 speex_suite (void)
 {
   Suite *s = suite_create ("speex");
-  TCase *tc_chain = tcase_create ("speex");
+  TCase *tc_chain;
 
-  suite_add_tcase (s, tc_chain);
+  suite_add_tcase (s, (tc_chain = tcase_create ("timestamping")));
   tcase_add_test (tc_chain, test_encoder_timestamp);
   tcase_add_test (tc_chain, test_encoder_to_decoder_timestamp);
+
+  suite_add_tcase (s, (tc_chain = tcase_create ("headers")));
+  tcase_add_test (tc_chain, test_headers_in_caps);
+  tcase_add_test (tc_chain, test_headers_in_buffers);
+  tcase_add_test (tc_chain, test_headers_not_in_buffers);
+  tcase_add_test (tc_chain, test_headers_from_flv);
 
   return s;
 }
