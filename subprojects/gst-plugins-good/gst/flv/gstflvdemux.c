067229f245fb18a043e64e781d133be78b611ffa
Havard Graff <havard.graff@gmail.com>
2016-03-26 11:06:33 +0100

    flvdemux: Set no-more-pads-threshold default to -1 [pexhack]
    This is a Pexip specific change since we are only interested in using
    gstflvdemux live.

--- subprojects/gst-plugins-good/gst/flv/gstflvdemux.c
+++ subprojects/gst-plugins-good/gst/flv/gstflvdemux.c
@@ -106,7 +106,7 @@ GST_ELEMENT_REGISTER_DEFINE_WITH_CODE (flvdemux, "flvdemux",
 #define RESYNC_THRESHOLD 2000
 
 /* how much stream time to wait for audio tags to appear after we have video, or vice versa */
-#define DEFAULT_NO_MORE_PADS_THRESHOLD (6 * GST_SECOND)
+#define DEFAULT_NO_MORE_PADS_THRESHOLD -1
 
 static gboolean flv_demux_handle_seek_push (GstFlvDemux * demux,
     GstEvent * event);
