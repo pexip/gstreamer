c92e0654c46ed30f805e554826692c9d56e9fca2
Stian Selnes <stian@pexip.com>
2019-01-28 17:19:16 +0100

    pcapparse: Take control of the discont flag, don't copy from input
    Take control of the discont flag and set it only on the first buffer.
    Don't copy from the incoming buffer since that may set the discont
    flag for many consecutive packets if the incoming buffer contains
    multiple packets.

--- subprojects/gst-plugins-bad/gst/pcapparse/gstpcapparse.c
+++ subprojects/gst-plugins-bad/gst/pcapparse/gstpcapparse.c
@@ -123,6 +123,7 @@ struct _GstPcapParse
 
   gboolean newsegment_sent;
   gboolean first_packet;
+  gboolean discont;
 };
 
 
@@ -171,6 +172,7 @@ gst_pcap_parse_reset (GstPcapParse * self)
   self->start_ts = GST_CLOCK_TIME_NONE;
   self->newsegment_sent = FALSE;
   self->first_packet = TRUE;
+  self->discont = TRUE;
 
   gst_adapter_clear (self->adapter);
   g_hash_table_remove_all (self->stats_map);
@@ -591,6 +593,15 @@ gst_pcap_parse_chain (GstPad * pad, GstObject * parent, GstBuffer * buffer)
 
             if (list == NULL)
               list = gst_buffer_list_new ();
+
+            /* Take control of the discont flag in order to not have one
+             * discont buffer be split into multiple  */
+            if (self->discont)
+              GST_BUFFER_FLAG_SET (out_buf, GST_BUFFER_FLAG_DISCONT);
+            else
+              GST_BUFFER_FLAG_UNSET (out_buf, GST_BUFFER_FLAG_DISCONT);
+            self->discont = FALSE;
+
             gst_buffer_list_add (list, out_buf);
           } else {
             gst_adapter_unmap (self->adapter);
