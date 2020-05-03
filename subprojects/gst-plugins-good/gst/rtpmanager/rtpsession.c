f8c391d42f34fe26dfdcb7ed785012f3e0a5633b
Havard Graff <havard@pexip.com>
2020-05-03 13:26:01 +0200

    rtpsession: downgrade empty RTCP error to info

--- subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.c
+++ subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.c
@@ -4968,7 +4968,7 @@ done:
   }
 
   if (all_empty)
-    GST_ERROR ("generated empty RTCP messages for all the sources");
+    GST_INFO ("generated empty RTCP messages for all the sources");
 
   /* schedule remaining nacks */
   RTP_SESSION_LOCK (sess);
