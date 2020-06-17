90dd4a230290f342c9b29d9230f157bbb8487efe
Mikhail Fludkov <fludkov.me@gmail.com>
2020-06-17 13:54:05 +0200

    gstpad: warn not linked flow errors [pexhack]

--- subprojects/gstreamer/gst/gstpad.c
+++ subprojects/gstreamer/gst/gstpad.c
@@ -149,6 +149,8 @@ struct _GstPadPrivate
    * by a single thread at a time. Protected by the object lock */
   GCond activation_cond;
   gboolean in_activation;
+
+  gboolean warned_unlinked;
 };
 
 typedef struct
@@ -221,6 +223,18 @@ static GstFlowQuarks flow_quarks[] = {
   {GST_FLOW_NOT_SUPPORTED, "not-supported", 0},
   {GST_FLOW_CUSTOM_ERROR, "custom-error", 0}
 };
+
+static void
+gst_pad_warn_if_unlinked (GstPad *pad, GstFlowReturn res) {
+  if (G_UNLIKELY (GST_PAD_PEER (pad) == NULL && res == GST_FLOW_NOT_LINKED)) {
+    if (!pad->priv->warned_unlinked) {
+      pad->priv->warned_unlinked = TRUE;
+      GST_WARNING_OBJECT (pad, "%p:%p returning not-linked", GST_PAD_PARENT(pad), pad);
+    }
+  } else {
+    pad->priv->warned_unlinked = FALSE;
+  }
+}
 
 /**
  * gst_flow_get_name:
@@ -416,6 +430,7 @@ gst_pad_init (GstPad * pad)
   pad->priv->events = g_array_sized_new (FALSE, TRUE, sizeof (PadEvent), 16);
   pad->priv->events_cookie = 0;
   pad->priv->last_cookie = -1;
+  pad->priv->warned_unlinked = FALSE;
   g_cond_init (&pad->priv->activation_cond);
 
   pad->ABI.abi.last_flowret = GST_FLOW_FLUSHING;
@@ -4847,6 +4862,7 @@ gst_pad_push (GstPad * pad, GstBuffer * buffer)
   GST_TRACER_PAD_PUSH_PRE (pad, buffer);
   res = gst_pad_push_data (pad,
       GST_PAD_PROBE_TYPE_BUFFER | GST_PAD_PROBE_TYPE_PUSH, buffer);
+  gst_pad_warn_if_unlinked (pad, res);
   GST_TRACER_PAD_PUSH_POST (pad, res);
   return res;
 }
@@ -4887,6 +4903,7 @@ gst_pad_push_list (GstPad * pad, GstBufferList * list)
   GST_TRACER_PAD_PUSH_LIST_PRE (pad, list);
   res = gst_pad_push_data (pad,
       GST_PAD_PROBE_TYPE_BUFFER_LIST | GST_PAD_PROBE_TYPE_PUSH, list);
+  gst_pad_warn_if_unlinked (pad, res);
   GST_TRACER_PAD_PUSH_LIST_POST (pad, res);
   return res;
 }
@@ -5283,6 +5300,7 @@ probe_stopped_unref:
   }
 done:
   GST_TRACER_PAD_PULL_RANGE_POST (pad, NULL, ret);
+  gst_pad_warn_if_unlinked (pad, ret);
   return ret;
 }
 
