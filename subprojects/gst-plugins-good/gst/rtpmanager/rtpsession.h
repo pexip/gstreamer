413be2cee0601fc7be2cff3d5d0f4f2ea24bcfc8
John-Mark Bell <jmb@pexip.com>
2020-12-16 16:36:27 +0000

    rtptwcc: use interval-based feedback on RTCP thread
    This decouples the TWCC feedback reporting from the incoming RTP
    packet flow; instead, scheduling TWCC feedback reporting from the
    RTCP thread.

--- subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.h
+++ subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.h
@@ -279,6 +279,7 @@ struct _RTPSession {
   gboolean      allow_early;
 
   GstClockTime  next_early_rtcp_time;
+  GstClockTime  next_twcc_rtcp_time;
 
   gboolean      sr_req_pending;
   gboolean      scheduled_bye;
