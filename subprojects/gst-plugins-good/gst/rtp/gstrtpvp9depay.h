1b3847dd207a91d727e6d870b7054727505dc5ed
Havard Graff <havard@pexip.com>
2021-09-02 13:54:25 +0200

    rtpvp9depay: revert to pexip-changes (for now)

--- subprojects/gst-plugins-good/gst/rtp/gstrtpvp9depay.h
+++ subprojects/gst-plugins-good/gst/rtp/gstrtpvp9depay.h
@@ -40,6 +40,7 @@ G_BEGIN_DECLS
 #define GST_RTP_VP9_DEPAY_GET_CLASS(obj) \
   (G_TYPE_INSTANCE_GET_CLASS ((obj), GST_TYPE_RTP_VP9_DEPAY, \
     GstRtpVP9DepayClass))
+#define GST_RTP_VP9_DEPAY_CAST(obj) ((GstRtpVP9Depay *)(obj))
 
 typedef struct _GstRtpVP9Depay GstRtpVP9Depay;
 typedef struct _GstRtpVP9DepayClass GstRtpVP9DepayClass;
@@ -61,6 +62,8 @@ struct _GstRtpVP9Depay
   gint last_height;
   guint last_picture_id;
   GstEvent *last_lost_event;
+  gboolean last_pushed_was_lost_event;
+  gboolean caps_sent;
   /* In between pictures, we might store GstRTPPacketLost events instead
    * of forwarding them immediately, we check upon reception of a new
    * picture id whether a gap was introduced, in which case we do forward
@@ -70,11 +73,8 @@ struct _GstRtpVP9Depay
   gboolean stop_lost_events;
   gboolean inter_picture;
 
-  gboolean waiting_for_keyframe;
-
-  /* Properties */
-  gboolean wait_for_keyframe;
-  gboolean request_keyframe;
+  /* properties */
+  gboolean hide_picture_id_gap;
 };
 
 GType gst_rtp_vp9_depay_get_type (void);
