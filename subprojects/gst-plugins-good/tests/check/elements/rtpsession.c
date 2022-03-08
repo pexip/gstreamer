5f3471d80306c13c63f19a6b1ab33dc2040f4de9
Tulio Beloqui <tulio@pexip.com>
2022-03-08 11:02:17 -0300

    rtptwcc: prune sent packets also by a maximum of 65536 to avoid overflow
    We cannot *only* depend on the feedback message to prune sent packets,
    since a miss functional receiver (those that doesn't generate a feedback on
    a proper rate) can potentially cause us to overflow the history
    of sent packets.
    
    The maximum is defined by the number of packets (using 16 bits) a twcc
    feedback message can contain status for.

--- subprojects/gst-plugins-good/tests/check/elements/rtpsession.c
+++ subprojects/gst-plugins-good/tests/check/elements/rtpsession.c
@@ -35,6 +35,16 @@
 #include <gst/net/gstnetaddressmeta.h>
 #include <gst/video/video.h>
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_VALGRIND_VALGRIND_H
+# include <valgrind/valgrind.h>
+#else
+# define RUNNING_ON_VALGRIND 0
+#endif
+
 #define TEST_BUF_CLOCK_RATE 8000
 #define TEST_BUF_PT 0
 #define TEST_BUF_SSRC 0x01BADBAD
@@ -4517,6 +4527,55 @@ GST_START_TEST (test_twcc_feedback_old_seqnum)
 
 GST_END_TEST;
 
+GST_START_TEST (test_twcc_feedback_max_sent_packets)
+{
+  /* test is very intense for valgrind  or debug build */
+  if (RUNNING_ON_VALGRIND || g_getenv ("EXECUTING_UNDER_DEBUG_BUILD"))
+    return;
+
+  SessionHarness *h = session_harness_new ();
+  guint i;
+
+  guint8 fci[] = {
+    0x00, 0x00,                 /* base sequence number: 0 */
+    0xff, 0xff,                 /* packet status count: 65535 */
+    0x00, 0x00, 0x00,           /* reference time: 0 */
+    0x00,                       /* feedback packet count: 0 */
+    0x1f, 0xff,                 /* (9x) run-length with max length as not recv: 0 0 0 1 1 1 1 1 | 1 1 1 1 1 1 1 1 */
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+    0x1f, 0xff,
+  };
+
+  session_harness_set_twcc_send_ext_id (h, TEST_TWCC_EXT_ID);
+
+  /* send over 65536 packets */
+  for (i = 0; i < 65536 * 2; i++)
+    session_harness_send_rtp (h, generate_twcc_send_buffer (i, FALSE));
+
+  /* receive the feedback message and verify we have sent out 65535 packets,
+     which internally means that we only keep a history of the last 65536
+     sent packets */
+  session_harness_recv_rtcp (h,
+      generate_twcc_feedback_rtcp (fci, sizeof (fci)));
+  twcc_verify_stats (h, 0, 0, 65535, 0, 0.0f, GST_CLOCK_STIME_NONE);
+
+  /* after this, the history is cleaned up, so the same feedback message will
+     get us only 2 sent packets */
+  session_harness_recv_rtcp (h,
+      generate_twcc_feedback_rtcp (fci, sizeof (fci)));
+  twcc_verify_stats (h, 0, 0, 2, 0, 0.0f, GST_CLOCK_STIME_NONE);
+
+  session_harness_free (h);
+}
+
+GST_END_TEST;
+
 GST_START_TEST (test_twcc_non_twcc_pkts_does_not_mark_loss)
 {
   SessionHarness *h_send = session_harness_new ();
@@ -5175,6 +5234,7 @@ rtpsession_suite (void)
   tcase_add_test (tc_chain, test_twcc_feedback_interval_new_internal_source);
   tcase_add_test (tc_chain, test_twcc_feedback_count_wrap);
   tcase_add_test (tc_chain, test_twcc_feedback_old_seqnum);
+  tcase_add_test (tc_chain, test_twcc_feedback_max_sent_packets);
   tcase_add_test (tc_chain, test_twcc_non_twcc_pkts_does_not_mark_loss);
 
   tcase_add_test (tc_chain, test_send_rtcp_instantly);
