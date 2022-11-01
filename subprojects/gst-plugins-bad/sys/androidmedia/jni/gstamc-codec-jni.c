e35b81f1814a6848017bfbb4548741ef0e7e9738
Tulio Beloqui <tulio@pexip.com>
2022-11-01 15:21:03 +0100

    gstamc-codec-jni: added NULL check before unrefing codec->surface

--- subprojects/gst-plugins-bad/sys/androidmedia/jni/gstamc-codec-jni.c
+++ subprojects/gst-plugins-bad/sys/androidmedia/jni/gstamc-codec-jni.c
@@ -586,7 +586,9 @@ gst_amc_codec_configure (GstAmcCodec * codec, GstAmcFormat * format,
   env = gst_amc_jni_get_env ();
 
   if (surface) {
-    g_object_unref (codec->surface);
+    if (codec->surface)
+      g_object_unref (codec->surface);
+
     codec->surface =
         gst_amc_surface_new ((GstAmcSurfaceTextureJNI *) surface, err);
     if (!codec->surface)
