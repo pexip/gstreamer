4ef49f4209e7eee9d88ca1830ffb0d6b881e014b
Tulio Beloqui <tulio@pexip.com>
2023-07-11 18:37:55 +0200

    audiodecoder: do not account for upstream latency when producing DTX packets
    - Removed the upstream latency as well, not needed anymore.
    
    (cherry picked from commit 86d1d53c4d5045b9b5b776d756ec2b8825e640a8)

--- subprojects/gst-plugins-base/gst-libs/gst/audio/gstaudiodecoder.c
+++ subprojects/gst-plugins-base/gst-libs/gst/audio/gstaudiodecoder.c
@@ -262,9 +262,6 @@ struct _GstAudioDecoderPrivate
   /* context storage */
   GstAudioDecoderContext ctx;
 
-  /* the cached upstream latency */
-  GstClockTime us_latency;
-
   /* properties */
   GstClockTime latency;
   GstClockTime reported_min_latency;
@@ -1720,9 +1717,7 @@ gst_audio_decoder_dtx_task_func (gpointer user_data)
     GstFlowReturn ret;
     GstBuffer *buf;
 
-    pts += dec->priv->us_latency;
     pts += dec->priv->last_buffer_duration;
-
 
     gst_audio_decoder_dtx_wait_unlocked (dec, base_time + pts);
 
@@ -3209,8 +3204,6 @@ gst_audio_decoder_src_query_default (GstAudioDecoder * dec, GstQuery * query)
         else
           max_latency += dec->priv->ctx.max_latency;
 
-        /* store the upstream min latency */
-        dec->priv->us_latency = min_latency;
         GST_OBJECT_UNLOCK (dec);
 
         gst_query_set_latency (query, live, min_latency, max_latency);
