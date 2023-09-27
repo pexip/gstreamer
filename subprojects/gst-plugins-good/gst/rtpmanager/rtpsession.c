a0f02b5c6abe8ce678991c4c5bc190c8b6b91436
Håvard Graff <havard.graff@gmail.com>
2023-09-27 11:17:39 +0200

    rtpsession: Add property "stats-min-interval" and notify "stats"
    Notify about changed stats when RTCP is sent. Since notifying on every
    RTCP packet (when early RTCP is used) may introduce significant
    overhead, so the property "stats-min-interval" is added in order to
    control how often this notification can happen.

--- subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.c
+++ subprojects/gst-plugins-good/gst/rtpmanager/rtpsession.c
@@ -86,6 +86,7 @@ enum
 #define DEFAULT_TWCC_FEEDBACK_INTERVAL GST_CLOCK_TIME_NONE
 #define DEFAULT_UPDATE_NTP64_HEADER_EXT TRUE
 #define DEFAULT_TIMEOUT_INACTIVE_SOURCES TRUE
+#define DEFAULT_STATS_NOTIFY_MIN_INTERVAL   0
 
 enum
 {
@@ -109,6 +110,7 @@ enum
   PROP_MAX_DROPOUT_TIME,
   PROP_MAX_MISORDER_TIME,
   PROP_STATS,
+  PROP_STATS_NOTIFY_MIN_INTERVAL,
   PROP_RTP_PROFILE,
   PROP_RTCP_REDUCED_SIZE,
   PROP_RTCP_DISABLE_SR_TIMESTAMP,
@@ -674,6 +676,13 @@ rtp_session_class_init (RTPSessionClass * klass)
       "Various statistics", GST_TYPE_STRUCTURE,
       G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
 
+  properties[PROP_STATS_NOTIFY_MIN_INTERVAL] =
+      g_param_spec_uint ("stats-notify-min-interval",
+      "Stats notify minimum interval",
+      "Minimum interval between emitting notify signal for stats (in ms)",
+      0, G_MAXUINT, DEFAULT_STATS_NOTIFY_MIN_INTERVAL,
+      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
   properties[PROP_RTP_PROFILE] =
       g_param_spec_enum ("rtp-profile", "RTP Profile",
       "RTP profile to use for this session", GST_TYPE_RTP_PROFILE,
@@ -846,6 +855,9 @@ rtp_session_init (RTPSession * sess)
   sess->rtp_profile = DEFAULT_RTP_PROFILE;
   sess->reduced_size_rtcp = DEFAULT_RTCP_REDUCED_SIZE;
   sess->timestamp_sender_reports = !DEFAULT_RTCP_DISABLE_SR_TIMESTAMP;
+
+  sess->last_stats_notify_time = GST_CLOCK_TIME_NONE;
+  sess->stats_notify_min_interval_ms = DEFAULT_STATS_NOTIFY_MIN_INTERVAL;
 
   sess->is_doing_ptp = TRUE;
 
@@ -1047,6 +1059,9 @@ rtp_session_set_property (GObject * object, guint prop_id,
     case PROP_MAX_MISORDER_TIME:
       sess->max_misorder_time = g_value_get_uint (value);
       break;
+    case PROP_STATS_NOTIFY_MIN_INTERVAL:
+      sess->stats_notify_min_interval_ms = g_value_get_uint (value);
+      break;
     case PROP_RTP_PROFILE:
       sess->rtp_profile = g_value_get_enum (value);
       /* trigger reconsideration */
@@ -1155,6 +1170,9 @@ rtp_session_get_property (GObject * object, guint prop_id,
       break;
     case PROP_STATS:
       g_value_take_boxed (value, rtp_session_create_stats (sess));
+      break;
+    case PROP_STATS_NOTIFY_MIN_INTERVAL:
+      g_value_set_uint (value, sess->stats_notify_min_interval_ms);
       break;
     case PROP_RTP_PROFILE:
       g_value_set_enum (value, sess->rtp_profile);
@@ -5064,7 +5082,12 @@ done:
 
   /* notify about updated statistics */
   if (!twcc_only) {
-    g_object_notify_by_pspec (G_OBJECT (sess), properties[PROP_STATS]);
+    if (!GST_CLOCK_TIME_IS_VALID (sess->last_stats_notify_time) ||
+        (GST_CLOCK_DIFF (sess->last_stats_notify_time, current_time) >=
+            sess->stats_notify_min_interval_ms * GST_MSECOND)) {
+      g_object_notify_by_pspec (G_OBJECT (sess), properties[PROP_STATS]);
+      sess->last_stats_notify_time = current_time;
+    }
   }
 
   /* push out the RTCP packets */
