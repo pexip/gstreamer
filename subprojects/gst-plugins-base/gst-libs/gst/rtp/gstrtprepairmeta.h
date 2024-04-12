d62f07a51a0f94694af021a9038b1a8923f7eea5
Misha Baranov <mikhail.baranov@pexip.com>
2024-04-12 16:46:52 +0200

    TWCCMan evaluates recovery pct for FEC
    Major rework of twcc manager internals
    caused by need to evaluate recovery
    percentage in case of FEC.
    
    Sent Packet structs are stored
    GstQueueArray to keep their pointers
    persistent. TWCC feedback parser creates
    a list of packets updated by this
    feedback, which then is passed into
    statistic gathering context.
    
    Then the remaining work is done in
    get_windowed_stats call.

---
+++ subprojects/gst-plugins-base/gst-libs/gst/rtp/gstrtprepairmeta.h
@@ -0,0 +1,74 @@
+/* GStreamer
+ * Copyright (C) <2024> Mikhail Baranov <mikhail.baranov@pexip.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_RTP_REPAIR_META_H__
+#define __GST_RTP_REPAIR_META_H__
+
+#include <gst/gst.h>
+#include <glib.h>
+#include <gst/rtp/rtp-prelude.h>
+
+G_BEGIN_DECLS
+
+#define GST_RTP_REPAIR_META_API_TYPE  (gst_rtp_repair_meta_api_get_type())
+#define GST_RTP_REPAIR_META_INFO  (gst_rtp_repair_meta_get_info())
+typedef struct _GstRTPRepairMeta GstRTPRepairMeta;
+
+struct _GstRTPRepairMeta
+{
+  GstMeta meta;
+
+  guint16 idx_red_packets;
+  guint16 num_red_packets;
+  guint32 ssrc;
+  GArray *seqnums;
+};
+
+GST_RTP_API
+GType               gst_rtp_repair_meta_api_get_type     (void);
+
+GST_RTP_API
+GstRTPRepairMeta *gst_buffer_add_rtp_repair_meta(GstBuffer *buffer,
+      const guint16 idx_red_packets, const guint16 num_red_packets,
+      const guint32 ssrc, const guint16 *seqnum, guint seqnum_count);
+
+GST_RTP_API
+GstRTPRepairMeta *  gst_buffer_get_rtp_repair_meta       (GstBuffer * buffer);
+
+GST_RTP_API
+gboolean gst_buffer_repairs_seqnum(GstBuffer *buffer, guint16 seqnum, guint32 ssrc);
+
+GST_RTP_API
+gboolean gst_buffer_get_repair_seqnums(GstBuffer *buffer, guint32 * ssrc,
+    GArray ** seqnums);
+
+/* If this packet is a FEC/RTX packet, what is it sequential number a block */
+GST_RTP_API
+gint gst_buffer_get_repair_idx(GstBuffer *buffer);
+
+/* If this packet is a FEC/RTX packet, how many redundancy packets are in a block */
+GST_RTP_API
+gint gst_buffer_get_repair_num(GstBuffer *buffer);
+
+GST_RTP_API
+const GstMetaInfo * gst_rtp_repair_meta_get_info         (void);
+
+G_END_DECLS
+
+#endif /* __GST_RTP_REPAIR_META_H__ */
