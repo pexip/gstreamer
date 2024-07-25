65ea2c08e1dd2c9e2296478f95032b9c3908d0ce
Tulio Beloqui <tulio@pexip.com>
2024-07-25 14:39:05 +0200

    gstbin: do the GST_ELEMENT_FLAG_NO_UNIQUE_CHECK on the bin, not the element you are adding
    We want to control that *all* elements on a bin skip the name check

--- subprojects/gstreamer/gst/gstbin.c
+++ subprojects/gstreamer/gst/gstbin.c
@@ -1183,7 +1183,7 @@ gst_bin_add_func (GstBin * bin, GstElement * element)
    * we can safely take the lock here. This check is probably bogus because
    * you can safely change the element name after this check and before setting
    * the object parent. The window is very small though... */
-  if (!GST_OBJECT_FLAG_IS_SET (element, GST_ELEMENT_FLAG_NO_UNIQUE_CHECK))
+  if (!GST_OBJECT_FLAG_IS_SET (bin, GST_ELEMENT_FLAG_NO_UNIQUE_CHECK))
     if (G_UNLIKELY (!gst_object_check_uniqueness (bin->children, elem_name)))
       goto duplicate_name;
 
