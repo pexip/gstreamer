22fb88cfbef409cd9c53538a5e130ea64b882450
Havard Graff <havard@pexip.com>
2025-02-05 15:54:07 +0100

    rtpmanager/rtpssrcdemux: ignore FLOW_EOS as well as NOT_LINKED
    Turns out that both can happen if you push on a pad that is being
    ripped away, so ignore them both.
    
    Also fixup test to compare a flow-return using int compare, since they
    are mostly negative!

--- subprojects/gst-plugins-good/gst/rtpmanager/gstrtpssrcdemux.c
+++ subprojects/gst-plugins-good/gst/rtpmanager/gstrtpssrcdemux.c
@@ -709,12 +709,13 @@ gst_rtp_ssrc_demux_chain (GstPad * pad, GstObject * parent, GstBuffer * buf)
   /* push to srcpad */
   ret = gst_pad_push (srcpad, buf);
 
-  if (ret == GST_FLOW_NOT_LINKED) {
-    /* on not-linked, this *could* happen because the ssrc got
+  if (ret == GST_FLOW_NOT_LINKED || ret == GST_FLOW_EOS) {
+    /* on not-linked and eos this *could* happen because the ssrc got
        removed while we were pushing, and since it can also be
        re-created, we can't know for sure if the removal was the
-       real reason, so simply ignore not-linked here */
-    GST_INFO_OBJECT (demux, "Ignoring not-linked flow return");
+       real reason, so simply ignore here */
+    GST_INFO_OBJECT (demux, "Ignoring flow return: %s",
+        gst_flow_get_name (ret));
 
     ret = GST_FLOW_OK;
   }
