6f8a0d34cfde19fcbc4ff024cc44f1ec2525e86e
Will Miller <will.miller@pexip.com>
2025-06-10 18:49:47 +0100

    plugins-bad/gstsctpdec: fix pad_name leaks
    Avoid leaking pad_name in error paths

--- subprojects/gst-plugins-bad/ext/sctp/gstsctpdec.c
+++ subprojects/gst-plugins-bad/ext/sctp/gstsctpdec.c
@@ -633,19 +633,14 @@ send_sticky_events (GstSctpDec * self, GstPad * pad, guint16 stream_id)
 }
 
 static GstPad *
-get_pad_for_stream_id (GstSctpDec * self, guint16 stream_id)
+create_pad (GstSctpDec * self, guint16 stream_id, const gchar * pad_name)
 {
-  GstPad *new_pad = NULL;
+  GstPad * new_pad;
   gint state;
-  gchar *pad_name;
   GstPadTemplate *template;
 
-  pad_name = g_strdup_printf ("src_%hu", stream_id);
-  new_pad = gst_element_get_static_pad (GST_ELEMENT (self), pad_name);
-  if (new_pad) {
-    g_free (pad_name);
-    return new_pad;
-  }
+  if (stream_id > MAX_STREAM_ID)
+    return NULL;
 
   GST_SCTP_DEC_ASSOC_MUTEX_LOCK (self);
   if (!self->sctp_association) {
@@ -666,13 +661,9 @@ get_pad_for_stream_id (GstSctpDec * self, guint16 stream_id)
 
   GST_DEBUG_OBJECT (self, "Creating new pad for stream id %u", stream_id);
 
-  if (stream_id > MAX_STREAM_ID)
-    return NULL;
-
   template = gst_static_pad_template_get (&src_template);
   new_pad = g_object_new (GST_TYPE_SCTP_DEC_PAD, "name", pad_name,
       "direction", template->direction, "template", template, NULL);
-  g_free (pad_name);
   gst_clear_object (&template);
 
   gst_pad_set_event_function (new_pad,
@@ -701,6 +692,18 @@ error_add:
 error_cleanup:
   gst_object_unref (new_pad);
   return NULL;
+}
+
+static GstPad *
+get_pad_for_stream_id (GstSctpDec * self, guint16 stream_id)
+{
+  gchar *pad_name = g_strdup_printf ("src_%hu", stream_id);
+  GstPad * pad = gst_element_get_static_pad (GST_ELEMENT (self), pad_name);
+  if (!pad) {
+    pad = create_pad (self, stream_id, pad_name);
+  }
+  g_free (pad_name);
+  return pad;
 }
 
 static void
