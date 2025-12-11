876665ab45d91a9ca3a24e4eafc34204681e887b
Sergei <sergei.kovalev@pexip.com>
2025-12-11 18:45:07 +0100

    gst-plugins-good/vpx: Add PEXIP_MODIFICATION to skip unsupported control V2
    This is additional fix for https://github.com/pexip/media/issues/1629
    This is part of the original fix that was missed in previous commit.

--- subprojects/gst-plugins-good/ext/vpx/gstvpxenc.c
+++ subprojects/gst-plugins-good/ext/vpx/gstvpxenc.c
@@ -1951,12 +1951,14 @@ gst_vpx_enc_set_format (GstVideoEncoder * video_encoder,
     GST_VPX_ENC_WARN (encoder, "Failed to set VP8E_SET_ENABLEAUTOALTREF",
         status);
   }
+#ifndef PEXIP_MODIFICATION
   status = vpx_codec_control (&encoder->encoder, VP8E_SET_NOISE_SENSITIVITY,
       encoder->noise_sensitivity);
   if (status != VPX_CODEC_OK) {
     GST_VPX_ENC_WARN (encoder, "Failed to set VP8E_SET_NOISE_SENSITIVITY",
         status);
   }
+#endif
   status = vpx_codec_control (&encoder->encoder, VP8E_SET_SHARPNESS,
       encoder->sharpness);
   if (status != VPX_CODEC_OK) {
@@ -1968,11 +1970,13 @@ gst_vpx_enc_set_format (GstVideoEncoder * video_encoder,
     GST_VPX_ENC_WARN (encoder, "Failed to set VP8E_SET_STATIC_THRESHOLD",
         status);
   }
+#ifndef PEXIP_MODIFICATION
   status = vpx_codec_control (&encoder->encoder, VP8E_SET_TOKEN_PARTITIONS,
       encoder->token_partitions);
   if (status != VPX_CODEC_OK) {
     GST_VPX_ENC_WARN (encoder, "Failed to set VP8E_SET_TOKEN_PARTIONS", status);
   }
+#endif
   status = vpx_codec_control (&encoder->encoder, VP8E_SET_ARNR_MAXFRAMES,
       encoder->arnr_maxframes);
   if (status != VPX_CODEC_OK) {
